3385e98719a31d150c2b85b282a0b32f8a0e6b12
jaya <jaijai28797@gmail.com>
2024-07-31 12:52:27 +0530

    Webhook

---
+++ webhook/webhook/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from Destination.views import AccountViewSet, DestinationViewSet, get_destinations, incoming_data
+
+router = DefaultRouter()
+router.register(r'accounts', AccountViewSet)
+router.register(r'destinations', DestinationViewSet)
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('accounts/<int:account_id>/destinations/', get_destinations, name='get_destinations'),
+    path('server/incoming_data/', incoming_data, name='incoming_data'),
+]
